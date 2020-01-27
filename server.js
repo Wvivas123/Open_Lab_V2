bcb63971ef24588dfc45527319f305826ef532c7
William Vivas <williamvivasdev@gmail.com>
2020-01-27 15:51:24 -0500

    Started commments on Server file

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
+//NPM Packages Used in the creation of app
+//_________________________________________//
 const express = require('express');
 const app = express();
 const multer = require('multer');
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
-app.use(cors());
 
+//______________________________________//
+
+//Initilizes Cors
+//______________________________________//
+app.use(cors());
+//______________________________________//
+
+//Declares storage var and saves location of where multer will save uploads
+//___________________________________________________________//
 let storage = multer.diskStorage({
   destination: function(req, file, cb) {
+    //uploads will be saved to Directory public_uploads
     cb(null, 'public_uploads');
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
   }
 });
+//___________________________________________________________//
 
+//Runs multer system storage function//
+//__________________________________________________________//
 const upload = multer({ storage: storage }).array('file');
+//_________________________________________________________//
 
-app.get('/', function(req, res) {
-  return res.send('Hello Server');
-});
-
+//Upload endpoint for upload functionality
+//_________________________________________________//
 app.post('/upload', function(req, res) {
   upload(req, res, function(err) {
     if (err instanceof multer.MulterError) {
@@ -34,7 +47,11 @@ app.post('/upload', function(req, res) {
     // Everything went fine.
   });
 });
+//_________________________________________________//
 
+//initilizes server and responds telling server is running
+//_________________________________________________________//
 app.listen(PORT, function() {
   console.log('Server running on port 8000');
 });
+//_______________________________________________________//
