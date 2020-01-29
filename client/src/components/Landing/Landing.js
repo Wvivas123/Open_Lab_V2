f3fe13dee4cdfda1304568079ddb27ff336f8f16
William Vivas <williamvivasdev@gmail.com>
2020-01-29 15:02:34 -0500

    Added links to Header

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello world</h1>
-        <h1>This is Open Lab a place to find collaborators</h1>
-      </div>
+      <section className="Landing">
+        <div>
+          <h1>Hello world</h1>
+          <h1>This is Open Lab a place to find collaborators</h1>
+          <button type="button" class="btn btn-success btn-block">
+            Click to upload file
+          </button>
+        </div>
+      </section>
     );
   }
 }
